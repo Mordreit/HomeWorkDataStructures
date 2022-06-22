521af270b65c9caae54c1d4c29a575c5b00e72b0
Mordreit <spirit_x93@mail.ru>
2022-06-22 17:40:00 +0400

    Готовое дз!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String str1 = ("Привет");
+        String str2 = ("Меня зовут Юрий");
+        String str3 = ("Я живу в Москве");
+        String str4 = ("i love Java");
+
+        LinkedList<String> bio = new LinkedList<>();
+        bio.add(str1);
+        bio.add(str2);
+        bio.add(str3);
+        bio.add(str4);
+
+        bio.remove(2);
+
+        System.out.println(bio);
+
+
+
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("Привет");
+        strings.add("меня");
+        strings.add("зовут");
+        strings.add("массив");
+
+        strings.remove(0);
+
+        System.out.println(strings);
+
+
     }
 }
