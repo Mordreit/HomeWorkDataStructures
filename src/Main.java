04f763457e4a343ddc4912d9ef8ffb465625f01a
Mordreit <spirit_x93@mail.ru>
2022-06-28 18:58:56 +0400

    Переделанное дз

--- src/Main.java
+++ src/Main.java
@@ -4,33 +4,41 @@ import java.util.LinkedList;
 public class Main {
     public static void main(String[] args) {
 
-        String str1 = ("Привет");
-        String str2 = ("Меня зовут Юрий");
-        String str3 = ("Я живу в Москве");
-        String str4 = ("i love Java");
+        LinkedList<Car> cars = new LinkedList<>();
+        Car lambo = new Car("Lamborghini Aventador");
+        Car ford = new Car("Ford Mustang");
+        Car reno = new Car("Reno Arkana");
+        Car kia = new Car("KIA Stinger");
+        Car mazda = new Car("Mazda rx7");
+        Car toyota = new Car("Toyota Supra");
+        Car lexus = new Car("Lexus rc");
 
-        LinkedList<String> bio = new LinkedList<>();
-        bio.add(str1);
-        bio.add(str2);
-        bio.add(str3);
-        bio.add(str4);
+        cars.add(lambo);
+        cars.add(ford);
+        cars.add(reno);
+        cars.add(kia);
+        cars.add(mazda);
+        cars.add(toyota);
+        cars.add(lexus);
 
-        bio.remove(2);
+        cars.remove(2);
 
-        System.out.println(bio);
-
-
-
-        ArrayList<String> strings = new ArrayList<>();
-        strings.add("Привет");
-        strings.add("меня");
-        strings.add("зовут");
-        strings.add("массив");
-
-        strings.remove(0);
-
-        System.out.println(strings);
+        System.out.println(cars);
 
 
+        ArrayList<Cat> cats = new ArrayList<>();
+        Cat pushok = new Cat("Пушок");
+        Cat pirat = new Cat("Пират");
+        Cat fedor = new Cat("Федор");
+        Cat lucifer = new Cat("Люцифер");
+
+        cats.add(pushok);
+        cats.add(pirat);
+        cats.add(fedor);
+        cats.add(lucifer);
+
+        cats.remove(2);
+
+        System.out.println(cats);
+        }
     }
-}
